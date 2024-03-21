bede3ae670c6c6b1d6429bce39ed9829b826b59d
Ksenia <ksenia.medvedeva.1993@gmail.com>
2024-03-21 22:01:19 +0500

    feat: methot arraysort

---
+++ src/main/java/ru/kseniamedvedeva/arrayapp2/entity/ArraySorting.java
@@ -0,0 +1,15 @@
+package ru.kseniamedvedeva.arrayapp2.entity;
+
+public class ArraySorting {
+    public static void arraySort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int k = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = k;
+                }
+            }
+        }
+    }
+}
