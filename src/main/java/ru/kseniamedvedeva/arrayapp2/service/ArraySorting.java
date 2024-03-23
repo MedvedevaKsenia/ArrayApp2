99793829aeb4a797904ce80d8bdd5f9022a69b42
Ksenia <ksenia.medvedeva.1993@gmail.com>
2024-03-23 23:13:41 +0500

    fixed method arraySort

--- src/main/java/ru/kseniamedvedeva/arrayapp2/service/ArraySorting.java
+++ src/main/java/ru/kseniamedvedeva/arrayapp2/service/ArraySorting.java
@@ -5,9 +5,9 @@ public class ArraySorting {
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[i] > arr[j]) {
-                    int k = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = k;
+                    arr[i] = arr[i] + arr[j];
+                    arr[j] = arr[i] - arr[j];
+                    arr[i] = arr[i] - arr[j];
                 }
             }
         }
